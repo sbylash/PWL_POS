ebda5c11aac18e11ba736c7d5edc1a8fc9f571a1
Shasia Sasa Salsabyla <salsabyla3008@gmail.com>
2024-03-01 20:14:29 +0700

    update [migration]

--- database/migrations/2024_03_01_032103_create_m_level_table.php
+++ database/migrations/2024_03_01_032103_create_m_level_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
+    
     public function down(): void
     {
         Schema::dropIfExists('m_level');
