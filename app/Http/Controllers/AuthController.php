a595dd2887755c1eccd2bfb85a86c7f654915948
Shasia Sasa Salsabyla <salsabyla3008@gmail.com>
2024-05-16 19:30:06 +0700

    update [jobsheet 9]

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
